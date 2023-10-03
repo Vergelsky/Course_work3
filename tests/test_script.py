780940141e7d8a17181af7e22f5cf3c4e2b4539f
Dmitry Vergelsky <graf.werger@gmail.com>
2023-10-04 00:04:07 +0700

    Создал заготовку тестовых функций

---
+++ tests/test_script.py
@@ -0,0 +1,27 @@
+from src.scripts import *
+
+
+
+def test_extract_operations_list(filename):
+    pass
+
+def test_filter_executed(operations_list):
+    pass
+
+def test_sort_for_date(operations_list):
+    pass
+
+def test_make_card_number(card_number):
+    pass
+
+
+def test_make_id_number(card_number):
+    pass
+
+
+def test_make_date(card_number):
+    pass
+
+
+def test_comb_text(operation):
+    pass
